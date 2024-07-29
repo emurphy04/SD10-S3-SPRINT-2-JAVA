325cfda98224562259d5863119af59f31d4bd302
Ethan Murphy <147887415+emurphy04@users.noreply.github.com>
2024-07-29 15:53:18 -0230

    Switched to IntelliJ, its better

--- UserService.java
+++ UserService.java
@@ -1,13 +1,44 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class UserService {
     public static void main(String[] args) {
-        String url="jdbc:mysql:"
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DriverManager.getConnection(null, null, null)
+        ArrayList<Items> Products = new ArrayList<Items>();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            String sql = "select * from products";
+            String url = "jdbc:postgresql://localhost:5432/Java";
+            String username = "postgres";
+            String password = "admin";
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()){
+                String itemName = rs.getString(1);
+                String itemSku = rs.getString(2);
+                double itemPrice = rs.getDouble(3);
+                String itemDesc = rs.getString(4);
+                String itemCat = rs.getString(5);
+
+                Products.add(new Items(itemName, itemSku, itemPrice, itemDesc, itemCat));
+            }
+
+            System.out.println(Products.get(1));
+
+
+
+            //make one of these for each value in the table and have it loop for each row, then in the loop create the
+            //items objects with those values and add them to an array list, the array list is goated
+
+
 
         } catch (Exception e) {
             System.out.println("Unable to establish a connection - "+e);
@@ -20,6 +51,8 @@ public class UserService {
         buyer1.getCart();
 
         buyer1.addToCart(c01);
+
+        buyer1.addToCart(Products.get(0));
 
         buyer1.getCart();
     }
