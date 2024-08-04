b082c20ddc5756623ad1a37c952c1131b0f3fa71
Ethan Murphy <147887415+emurphy04@users.noreply.github.com>
2024-08-04 20:16:53 -0230

    added Buyer CLI

--- UserService.java
+++ UserService.java
@@ -7,7 +7,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserService {
+
     public static void main(String[] args) {
+
+        ArrayList<Items> cart = new ArrayList<Items>();
 
         String role = "buyer";
 
@@ -41,8 +44,9 @@ public class UserService {
                 double itemPrice = rs.getDouble(3);
                 String itemDesc = rs.getString(4);
                 String itemCat = rs.getString(5);
+                String userListed = rs.getString(6);
 
-                Products.add(new Items(itemName, itemSku, itemPrice, itemDesc, itemCat));
+                Products.add(new Items(itemName, itemSku, itemPrice, itemDesc, itemCat, userListed));
             }
 
             System.out.println(Products.get(1));
@@ -171,6 +175,21 @@ public class UserService {
                     }
                     System.out.println("User created! Please login");
                     isLogged = true;
+                    switch (role) {
+                        case "admin":
+                            users.add(new Admin(userEnteredUsername, userEnteredPassword, userEnteredEmail));
+                            break;
+                        case "buyer":
+                            users.add(new Buyer(userEnteredUsername, userEnteredPassword, userEnteredEmail));
+                            break;
+                        case "seller":
+                            users.add(new Seller(userEnteredUsername, userEnteredPassword, userEnteredEmail));
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                    userIndex = users.size()-1;
                     break;
                 default:
                     System.out.println("Invalid Choice, Please try again.");
@@ -179,26 +198,234 @@ public class UserService {
         }
 
         //CLI
-        System.out.println(users.get(userIndex).getRole());
-        switch (users.get(userIndex).getRole()) {
-            case "admin":
-                System.out.println("admin");
-                //make admin stuff now
-                break;
+        boolean signout = false;
+        while(signout == false){
+            User user = users.get(userIndex);
+            switch (users.get(userIndex).getRole()) {
+                case "admin":
+                    //make admin stuff now
 
-            case "buyer":
-                System.out.println("buyer");
-                //made buyer stuff now
-                break;
+                    System.out.println("Admin Interface: ");
+                    System.out.println("================");
+                    System.out.println();
+                    System.out.println("1. List all Users");
+                    System.out.println("2. Delete user");
+                    System.out.println("3. List all products");
+                    System.out.println("4. Sign Out");
+                    System.out.println();
+                    System.out.println("Please enter your choice: ");
+                    String choice2 = scanner.nextLine();
 
-            case "seller":
-                System.out.println("seller");
-                //make seller stuff now
-                break;
-        
-            default:
-                break;
+                    switch (choice2) {
+                        case "1":
+                            System.out.println("---------------------------------------------------------------------------");
+                            for(int i = 0; i<users.size(); i++){
+                                System.out.println(users.get(i));
+                                System.out.println("---------------------------------------------------------------------------");
+                            }
+                            break;
+                        case "2":
+                            System.out.println("---------------------------------------------------------------------------");
+                            for(int i = 0; i<users.size(); i++){
+                                System.out.println(users.get(i));
+                                System.out.println("---------------------------------------------------------------------------");
+                            }
+                            System.out.println("Please enter the username of the user to be deleted: ");
+                            String delUser = scanner.nextLine();
+                            if(!(users.get(userIndex).username.equals(delUser))){
+                                for(int i = 0; i<users.size(); i++){
+                                    if(users.get(i).username.equals(delUser)){
+                                        System.out.println("Are you sure you want to delete this user? Y/N");
+                                        String choice3 = scanner.nextLine().toUpperCase();
+                                        if (choice3.equals("Y")) {
+                                            users.remove(i);
+                                            try {
+                                                Class.forName("org.postgresql.Driver");
+                                    
+                                                String url = "jdbc:postgresql://localhost:5432/Java";
+                                                String username = "postgres";
+                                                String password = "admin";
+                                    
+                                                Connection connection = DriverManager.getConnection(url, username, password);
+                        
+                                                PreparedStatement st = connection.prepareStatement("DELETE FROM users WHERE username=?");
+                                                st.setString(1, delUser);
+                                                st.executeUpdate();
+                                                st.close();
+                                    
+                                            } catch (Exception e) {
+                                                System.out.println("Unable to establish a connection - "+e);
+                                            }
+                                        } else {
+                                            System.err.println("Delete cancelled.");
+                                            break;
+                                        }
+
+                                        break;
+                                    }
+                                }
+                            } else {
+                                System.err.println("Cannot delete current user.");
+                            }
+                            break;
+                        case "3":
+                            System.out.println("---------------------------------------------------------------------------");
+                            for(int i = 0; i<Products.size(); i++){
+                                System.out.println(Products.get(i));
+                                System.out.println("---------------------------------------------------------------------------");
+                            }
+                            break;
+                        case "4":
+                            System.out.println("Exiting...");
+                            signout = true;
+                            break;
+                    
+                        default:
+                            break;
+                    }
+
+                    break;
+
+                case "buyer":
+                    //made buyer stuff now
+
+                    System.out.println("Customer Interface: ");
+                    System.out.println("================");
+                    System.out.println();
+                    System.out.println("1. Browse all products");
+                    System.out.println("2. Search for product");
+                    System.out.println("3. Add sku to cart");
+                    System.out.println("4. Checkout");
+                    System.out.println("5. Sign Out");
+                    System.out.println();
+                    System.out.println("Please enter your choice: ");
+                    choice2 = scanner.nextLine();
+
+                    switch (choice2) {
+                        case "1":
+                            System.out.println("---------------------------------------------------------------------------");
+                            for(int i = 0; i<Products.size(); i++){
+                                System.out.println(Products.get(i));
+                                System.out.println("---------------------------------------------------------------------------");
+                            }
+                            break;
+                        case "2":
+                            System.out.println("---------------------------------------------------------------------------");
+                            System.out.println("Search by:");
+                            System.out.println("1. Name");
+                            System.out.println("2. Category");
+                            System.out.println("3. Seller");
+                            String choice3 = scanner.nextLine();
+
+                            switch (choice3) {
+                                case "1":
+                                    System.out.println();
+                                    System.out.println("Search by name: ");
+                                    String search = scanner.nextLine();
+                                    System.out.println("---------------------------------------------------------------------------");
+                                    for (int i = 0; i < Products.size(); i++) {
+                                        if(Products.get(i).itemName.toLowerCase().indexOf(search.toLowerCase()) != -1){
+                                            System.out.println(Products.get(i));
+                                            System.out.println("---------------------------------------------------------------------------");
+                                        }
+                                    }
+                                    break;
+                                case "2":
+                                    System.out.println();
+                                    System.out.println("Search by category: ");
+                                    search = scanner.nextLine();
+                                    System.out.println("---------------------------------------------------------------------------");
+                                    for (int i = 0; i < Products.size(); i++) {
+                                        if(Products.get(i).itemCat.toLowerCase().indexOf(search.toLowerCase()) != -1){
+                                            System.out.println(Products.get(i));
+                                            System.out.println("---------------------------------------------------------------------------");
+                                        }
+                                    }
+                                    break;
+                                case "3":
+                                    System.out.println();
+                                    System.out.println("Search by seller: ");
+                                    search = scanner.nextLine();
+                                    System.out.println("---------------------------------------------------------------------------");
+                                    for (int i = 0; i < Products.size(); i++) {
+                                        if(Products.get(i).userListed.toLowerCase().indexOf(search.toLowerCase()) != -1){
+                                            System.out.println(Products.get(i));
+                                            System.out.println("---------------------------------------------------------------------------");
+                                        }
+                                    }
+                                    break;
+                            
+                                default:
+                                    break;
+                            }
+                            break;
+                        case "3":
+                            boolean cartLoop = true;
+                            while(cartLoop){
+                                System.out.println();
+                                System.out.println("Enter the sku you would like to add to your cart:");
+                                System.out.println();
+                                String sku = scanner.nextLine();
+                                for (int i = 0; i < Products.size(); i++) {
+                                    if(Products.get(i).itemSku.toLowerCase().equals(sku.toLowerCase())){
+                                        user.addToCart(Products.get(i));
+                                        System.out.println("Product added");
+                                    }
+                                }
+                                System.out.println();
+                                System.out.println("Would you like to add another item? Y/N");
+                                String choice4 = scanner.nextLine().toLowerCase();
+                                if (choice4.equals("n")){
+                                    cartLoop = false;
+                                }
+                            }
+                                break;
+                        case "4":
+                            double price = 0;
+                            System.out.println("---------------------------------------------------------------------------");
+                            System.out.println(user.username+"'s cart:");
+                            System.out.println();
+                            System.out.println("---------------------------------------------------------------------------");
+                            for (int i = 0; i < user.cart.size(); i++) {
+                                System.out.println(user.cart.get(i));
+                                price = price+user.cart.get(i).itemPrice;
+                                System.out.println("---------------------------------------------------------------------------");
+                            }
+                            System.out.println("Total Cost: $"+price);
+                            break;
+                        case "5":
+                            System.out.println("Exiting...");
+                            signout = true;
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    break;
+
+                case "seller":
+                    System.out.println("seller");
+                    //make seller stuff now
+
+                    System.out.println("Seller Interface: ");
+                    System.out.println("================");
+                    System.out.println();
+                    System.out.println("1. Browse all products");
+                    System.out.println("2. Search for product");
+                    System.out.println("3. Add to cart");
+                    System.out.println("4. Sell product");
+                    System.out.println("5. Delete listing");
+                    System.out.println("6. View own listings");
+                    System.out.println("7. Sign Out");
+                    System.out.println();
+                    System.out.println("Please enter your choice: ");
+                    choice2 = scanner.nextLine();
+                    break;
+            
+                default:
+                    break;
+            }
         }
-
     }
 }
